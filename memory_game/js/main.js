30e61990cf07c1abd94322a0e11636b3e5dda830
Colin Clancy <cmclancy8297@gmail.com>
2019-05-30 14:58:11 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,19 @@
 var	cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
- if (cardsInPlay.length===2) {console.log("Eureka")
- 	if (cardsInPlay[0]===cardsInPlay[1]) {alert("You found a match!");}
+function checkForMatch(){
+ 	if (cardsInPlay[0]===cardsInPlay[1]) {
+ 		console.log("You found a match!");}
  		else { 
  			alert("Try Again!");
  				}	
  }
+
+
+function flipCard(cardId){
+	console.log("User flipped "+ cards[cardId]);
+	cards.push(cardId);
+	checkForMatch();
+ 
+}
+flipCard(0);
+flipCard(2);
