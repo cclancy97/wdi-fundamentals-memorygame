97d6018182cab4cc0ffc3b5e675b0a8e210e332d
Colin Clancy <cmclancy8297@gmail.com>
2019-05-30 17:38:28 -0400

    added objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,29 @@
-var	cards = ["queen", "queen", "king", "king"];
+var	cards = [
+{
+		rank:"queen",
+		suit:"hearts",
+		cardImage: "images/queen-of-hearts.png"
+
+},
+{
+		rank:"queen",
+		suit:"diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+
+},
+{
+		rank:"king",
+		suit:"hearts",
+		cardImage: "images/king-of-hearts.png"
+
+},
+{
+		rank:"king",
+		suit:"diamonds",
+		cardImage: "images/king-of-diamonds.png"
+
+}
+];
 var cardsInPlay = [];
 function checkForMatch(){
  	if (cardsInPlay[0]===cardsInPlay[1]) {
@@ -10,8 +35,10 @@ function checkForMatch(){
 
 
 function flipCard(cardId){
-	console.log("User flipped "+ cards[cardId]);
-	cards.push(cardId);
+	console.log("User flipped "+ cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
  
 }
